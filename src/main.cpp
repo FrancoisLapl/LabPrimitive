b9d02fa63839f73c43f6eba83e8858a8f9d746ac
Frédéric-Simon Mimeault <frederic.simon.mimeault@gmail.com>
2017-05-17 15:39:48 -0400

    Update console logging

--- src/main.cpp
+++ src/main.cpp
@@ -210,7 +210,7 @@ ProcessImageBatch(int firstItemNb, int lastItemNb, char *character, FILE *fp, Ip
 
 		// Shows the feature vector at the screen
 		//printf(" : #%d %f %f\n\n", iNum, fVector[iNum][1], fVector[iNum][2]);
-		printf( "\n%d %f %f %f %f %f", iNum, fVector[iNum][1], fVector[iNum][2], fVector[iNum][3], fVector[iNum][4], fVector[iNum][5] );
+		printf( "%d %f %f %f %f %f\n", iNum, fVector[iNum][1], fVector[iNum][2], fVector[iNum][3], fVector[iNum][4], fVector[iNum][5] );
 
 		// And finally, store your features in a file
 		fprintf(fp, "%f,", fVector[iNum][1]);
